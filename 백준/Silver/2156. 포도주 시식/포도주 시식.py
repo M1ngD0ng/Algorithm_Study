fa59ed04ab399ebc67539c016f0b1228b8ece0cf
MinJeong LEE <60657536+M1ngD0ng@users.noreply.github.com>
2023-12-11 21:53:20 +0900

    Update 포도주 시식.py
    주석 추가

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -11,5 +11,5 @@ else:
   dp[1]=arr[0]+arr[1] 
   for i in range(2, N):
     mm=max(dp[i-3]+arr[i-1]+arr[i],dp[i-2]+arr[i]) 
-    dp[i]=max(dp[i-1],mm) 
+    dp[i]=max(dp[i-1],mm) # 이렇게 계산한것보다 그냥 이 칸을 건너뛰는게 더 높을 수도 있기 때문에...
   print(dp[N-1])
