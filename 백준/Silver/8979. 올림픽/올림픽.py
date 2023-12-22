63b8389112fc4b8333c07b91c9f2d0b376186541
MinJeong LEE <60657536+M1ngD0ng@users.noreply.github.com>
2023-12-22 19:43:08 +0900

    [Silver V] Title: 올림픽, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -2,22 +2,24 @@ import sys
 N,K=map(int,sys.stdin.readline().split())
 A=[ list(map(int,sys.stdin.readline().split())) for _ in range(N)]
 
-A=sorted(A,reverse=True,key=lambda x:(x[1], x[2],x[3]))
+A=sorted(A,reverse=True,key=lambda x:(x[1], x[2], x[3]))
 
 t=1
+A[0].append(1)
 if N==1:
   print(1)
 else:
   for i in range(1,N):
     if A[i][1]!=A[i-1][1]:
-      t+=1
+      t=i+1
     else:
       if A[i][2]!=A[i-1][2]:
-        t+=1
+        t=i+1
       else:
         if A[i][3]!=A[i-1][3]:
-          t+=1
+          t=i+1
+    A[i].append(t)
+  for i in range(N):
     if A[i][0]==K:
-      print(t)
-      break
-    
+      print(A[i][4])
+      break 
