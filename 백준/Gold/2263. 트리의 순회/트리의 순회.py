e30c703ce7b18bca7c3f0d54a7edf4ce664bf314
MinJeong LEE <60657536+M1ngD0ng@users.noreply.github.com>
2024-02-17 23:02:30 +0900

    [Gold I] Title: 트리의 순회, Time: 380 ms, Memory: 72364 KB -BaekjoonHub

---
+++ 백준/Gold/2263. 트리의 순회/트리의 순회.py
@@ -0,0 +1,31 @@
+
+# Inorder (중위 순회) : left Node -> root Node -> right Node
+# postorder (후위 순회) : left Node -> right Node -> root Node
+# preorder (전위 순회) : root Node -> left Node -> right Node
+
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 8)
+
+n = int(input())
+inorder = list(map(int, input().split()))
+postorder = list(map(int, input().split()))
+
+nodeNum = [0] * (n + 1)
+for i in range(n):
+    nodeNum[inorder[i]] = i
+
+def preorder(inStart, inEnd, postStart, postEnd):
+    if (inStart > inEnd) or (postStart > postEnd):
+        return
+    
+    root = postorder[postEnd]
+    
+    leftNode = nodeNum[root] - inStart
+    rightNode = inEnd - nodeNum[root]
+    
+    print(root, end = " ")
+    preorder(inStart, inStart + leftNode - 1, postStart, postStart + leftNode - 1)
+    preorder(inEnd - rightNode + 1, inEnd, postEnd - rightNode, postEnd - 1)
+
+preorder(0, n - 1, 0, n - 1)
