efd92f7fcc5efaa1fb252751ba5f6c01a9985442
MinJeong LEE <60657536+M1ngD0ng@users.noreply.github.com>
2024-02-22 17:35:50 +0900

    [Gold I] Title: 택배, Time: 64 ms, Memory: 34036 KB, Score: 52 point -BaekjoonHub

---
+++ 백준/Gold/8980. 택배/택배.py
@@ -0,0 +1,22 @@
+import sys
+from collections import defaultdict
+
+N,C=map(int,sys.stdin.readline().split())
+M=int(sys.stdin.readline())
+
+A=[list(map(int,sys.stdin.readline().split())) for _ in range(M)]
+
+A.sort(key=lambda x:(x[1]-x[0],-x[2]))
+dp=[0]*(N+1)
+
+ans=0
+for i in range(M):
+  pack=A[i][2]
+  for j in range(A[i][0],A[i][1]):
+    if dp[j]+pack>C:
+      pack=C-dp[j]
+  for j in range(A[i][0],A[i][1]):
+    dp[j]+=pack
+  ans+=pack
+  
+print(ans)
