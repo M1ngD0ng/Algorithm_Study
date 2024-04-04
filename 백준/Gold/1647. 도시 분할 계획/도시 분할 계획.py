91b9dcd3c57dff917bf05cd4d5dd4848afbde96d
MinJeong LEE <60657536+M1ngD0ng@users.noreply.github.com>
2024-04-04 17:00:35 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 2568 ms, Memory: 208396 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -0,0 +1,35 @@
+import sys
+sys.setrecursionlimit(10**5)
+
+N,M=map(int,sys.stdin.readline().split())
+cost=[]
+for _ in range(M):
+  a,b,c=map(int,sys.stdin.readline().split())
+  cost.append((a,b,c))
+
+cost.sort(key=lambda x:x[2])
+
+parent=[i for i in range(N+1)]
+
+def find(x):
+  if parent[x]!=x:
+    parent[x]=find(parent[x])
+  return parent[x]
+
+def union(a,b):
+  a=find(a)
+  b=find(b)
+  if a<b:
+    parent[b]=a
+  else:
+    parent[a]=b
+
+ans=0
+temp=0
+for a,b,c in cost:
+  if find(a)!=find(b):
+    union(a,b)
+    ans+=c
+    temp=max(temp,c)
+
+print(ans-temp)
