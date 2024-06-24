ba72034b968a4082f7063ec8f251ecb91e4dc570
MinJeong LEE <60657536+M1ngD0ng@users.noreply.github.com>
2024-06-25 01:30:05 +0900

    [Gold IV] Title: 최단경로, Time: 632 ms, Memory: 68220 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,33 +1,34 @@
 import sys
 import heapq
-
 V,E=map(int,sys.stdin.readline().split())
 K=int(sys.stdin.readline())
 graph=[[] for _ in range(V+1)]
 
 for _ in range(E):
-  a,b,c=map(int,sys.stdin.readline().split())
-  graph[a].append((b,c))
+    a,b,c=map(int,sys.stdin.readline().split())
+    graph[a].append((b,c))
 
-def dijkstra(start):
-  q=[]
-  heapq.heappush(q,(0,start))
-  distance=[1e8]*(V+1)
-  distance[start]=0
+def dijkstra(node):
+    q=[]
+    heapq.heappush(q, (0,node))
+    distance=[1e8]*(V+1)
+    distance[node]=0
 
-  while q:
-    dist,now=heapq.heappop(q)
-
-    if distance[now]<dist:
-      continue
-    for n,d in graph[now]:
-      if distance[n]<=(dist+d):
-        continue
-      distance[n]=dist+d
-      heapq.heappush(q,(dist+d,n)) 
-  for i in range(1,V+1):
-    if distance[i]==1e8:
-      print('INF')
-    else:
-      print(distance[i])
+    while q:
+        dist,n=heapq.heappop(q)
+        if dist>distance[n]:
+            continue
+        
+        for x,y in graph[n]:
+            if distance[x]<=dist+y:
+                continue
+            distance[x]=dist+y
+            heapq.heappush(q,(dist+y,x))
+    
+    for i in range(1,V+1):
+        if distance[i]==1e8:
+            print('INF')
+        else:
+            print(distance[i])
+    
 dijkstra(K)
