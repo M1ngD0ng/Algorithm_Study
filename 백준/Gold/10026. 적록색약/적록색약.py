e6c9fa6bc09bcf52c6231f1f68714885de35c470
MinJeong LEE <60657536+M1ngD0ng@users.noreply.github.com>
2024-06-25 02:01:59 +0900

    [Gold V] Title: 적록색약, Time: 76 ms, Memory: 34112 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,62 +1,57 @@
 import sys
-sys.setrecursionlimit(10**6)
+from collections import deque
 
-N = int(sys.stdin.readline())
-graph = [[] for _ in range(N)]
+N=int(sys.stdin.readline())
+graph=[list(sys.stdin.readline().rstrip()) for _ in range(N)]
 
-for i in range(N):
-	graph[i] = list(map(str, sys.stdin.readline().rstrip()))
-
-ans1=0
-ans2=0
-
-def inRange(nx,ny):
-  return nx>=0 and nx<N and ny>=0 and ny<N
-
-def dfs(x,y):
-  global visited
-  visited[x][y]=True
-  dx=[0,1,0,-1]
-  dy=[1,0,-1,0]
-
-  for i in range(4):
-    nx=x+dx[i]
-    ny=y+dy[i]
+def bfs(opt,x,y):
+    q=deque()
+    q.append((x,y))
+    visited[x][y]=True
+    dx,dy=[0,0,-1,1],[1,-1,0,0]
     
-    if not inRange(nx,ny) or visited[nx][ny]==True:
-      continue
-    else:
-      if graph[x][y]==graph[nx][ny]:
-        dfs(nx,ny)
+    while q:
+        vx,vy=q.popleft()
 
+        for k in range(4):
+            nx,ny=vx+dx[k],vy+dy[k]
+            if not (0<=nx<N and 0<=ny<N):
+                continue
+            if visited[nx][ny]:
+                continue
+            if opt==2:
+                if graph[x][y]=='B':
+                    if graph[nx][ny]!='B':
+                        continue
+                else:
+                    if graph[nx][ny]=='B':
+                        continue
+            else:
+                if graph[x][y]!=graph[nx][ny]:
+                    continue
+            visited[nx][ny]=True
+            q.append((nx,ny))
+            
+        
+            
 
-def cal():
-  global visited
-  x,y=0,0
-  ans=0
-  while True:
-    dfs(x,y)
-    ans+=1
-    for i in range(N):
-      if False in visited[i]:
-        j=visited[i].index(False)
-        nx,ny=i,j
-        break
-    if x!=nx or y!=ny:
-      x,y=nx,ny
-      continue
-    else:
-      break
-  return ans
-
-visited=[[False for _ in range(N)] for _ in range(N)]
-ans1=cal()
+visited=[[False]*N for _ in range(N)]
+cnt=0
 
 for i in range(N):
-  if 'G' in graph[i]:
     for j in range(N):
-      if graph[i][j]=='G':
-        graph[i][j]='R'
-visited=[[False for _ in range(N)] for _ in range(N)]
-ans2=cal()
-print(ans1,ans2)
+        if not visited[i][j]:
+            bfs(1,i,j)
+            cnt+=1
+
+sys.stdout.write(str(cnt)+' ')
+
+visited=[[False]*N for _ in range(N)]
+cnt=0
+for i in range(N):
+    for j in range(N):
+        if not visited[i][j]:
+            bfs(2,i,j)
+            cnt+=1
+
+sys.stdout.write(str(cnt)+'\n')
