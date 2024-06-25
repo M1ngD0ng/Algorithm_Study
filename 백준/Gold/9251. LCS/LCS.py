59a2ed2c649efc4a0a733a840467c5fbce5c75d6
MinJeong LEE <60657536+M1ngD0ng@users.noreply.github.com>
2024-06-26 01:28:48 +0900

    [Gold V] Title: LCS, Time: 524 ms, Memory: 55708 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,16 +1,17 @@
 import sys
+
 A=list(sys.stdin.readline().rstrip())
 B=list(sys.stdin.readline().rstrip())
 N,M=len(A),len(B)
 dp=[[0]*(M+1) for _ in range(N+1)]
-ans=0
-for i in range(1,N+1):
-  for j in range(1,M+1):
-    if A[i-1]==B[j-1]:
-      dp[i][j]=dp[i-1][j-1]+1
-    else:
-      dp[i][j]=max(dp[i-1][j],dp[i][j-1])
 
+for i in range(1,N+1):
+    for j in range(1,M+1):
+        if A[i-1]==B[j-1]:
+            dp[i][j]=dp[i-1][j-1]+1
+        else:
+            dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+ans=0
 for i in range(1,N+1):
   ans=max(ans,max(dp[i]))
 
