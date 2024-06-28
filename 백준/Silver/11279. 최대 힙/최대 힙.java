47826ab9c648d76c9bb0b1c0e9bb2b368e7f538f
MinJeong LEE <60657536+M1ngD0ng@users.noreply.github.com>
2024-06-28 21:05:26 +0900

    [Silver II] Title: 최대 힙, Time: 296 ms, Memory: 26328 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pQ = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int i=0;i<N;i++){
+            int x = Integer.parseInt(br.readLine());
+            if(x==0){
+                if(!pQ.isEmpty()){
+                    bw.write(String.valueOf(pQ.poll()));
+                    bw.newLine();
+                }else {
+                    bw.write("0\n");
+                }
+            }else{
+                pQ.offer(x);
+            }
+        }
+        bw.flush();
+    }
+}
