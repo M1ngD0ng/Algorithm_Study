c19b1c18480da0e349aeb8d3575f8c9ca037bed9
MinJeong LEE <60657536+M1ngD0ng@users.noreply.github.com>
2025-05-13 18:45:04 +0900

    [Gold III] Title: 파티, Time: 144 ms, Memory: 112120 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -2,37 +2,35 @@ import sys
 import heapq
 
 N,M,X=map(int,sys.stdin.readline().split())
-graph=[ [] for _ in range(N+1)]
+
+graph1=[[] for _ in range(N+1)]
+graph2=[[] for _ in range(N+1)]
 
 for _ in range(M):
   a,b,c=map(int,sys.stdin.readline().split())
-  graph[a].append((b,c))
+  graph1[a].append((b,c))
+  graph2[b].append((a,c))
 
-def dijkstra(start,end):
+toX=[1e8]*(N+1)
+fromX=[1e8]*(N+1)
+
+def dijkstra(graph, lst):
   q=[]
-  heapq.heappush(q,(0,start))
-  distance=[int(1e8)]*(N+1)
+  heapq.heappush(q,(0,X))
+  lst[X]=0
 
   while q:
-    dist,now=heapq.heappop(q)
-    
-    if distance[now]<dist:
-      continue
-    for i in graph[now]:
-      if i==start:
-        continue
-      if dist+i[1]<distance[i[0]]:
-        distance[i[0]]=dist+i[1]
-        heapq.heappush(q,(dist+i[1],i[0]))
-  if start==X and end==X:
-    return 0
-  else:
-    return distance[end]
+    cost, node=heapq.heappop(q)
 
-total=[0]
+    for nxt,dist in graph[node]:
+      if lst[nxt]>dist+cost:
+        lst[nxt]=dist+cost
+        heapq.heappush(q,(dist+cost,nxt))
+
+dijkstra(graph1,toX)
+dijkstra(graph2,fromX)
+
+ans=0
 for i in range(1,N+1):
-  t1=dijkstra(i,X)
-  t2=dijkstra(X,i)
-  total.append(t1+t2)
-
-print(max(total))
+  ans=max(ans,toX[i]+fromX[i])
+print(ans)
